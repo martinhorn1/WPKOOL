1a40da688c539cee4629ee6ca8b30d203cdfd33a
martinhorniktkhk <martin.horn@khk.ee>
2018-12-18 09:01:56 +0200

    modified file `config.php` (after activation of `Call Now Button` version 0.3.5)

--- wp-content/wflogs/config.php
+++ wp-content/wflogs/config.php
@@ -3,4 +3,4 @@
 This file is used by the Wordfence Web Application Firewall. Read 
 more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
 ******************************************************************
-a:5:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1544997286;s:7:"authKey";s:64:"cj9AO{zW,[kW00Un!FQK#15MKepwFUJpN]q!GSyP(mNyMT]QQr<39i.$]S&U.<5A";s:7:"version";s:5:"1.0.4";}
+a:5:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"cj9AO{zW,[kW00Un!FQK#15MKepwFUJpN]q!GSyP(mNyMT]QQr<39i.$]S&U.<5A";s:7:"version";s:5:"1.0.4";s:13:"attackDataKey";i:1810;}
